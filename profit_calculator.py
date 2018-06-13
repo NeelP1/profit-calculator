e7517ec88b259d946a5b69d3314ba35bb706e2d7
Neel Patel <npatel@Neels-MacBook-Pro.local>
2018-06-13 11:59:53 +1000

    Add todo for argparse implementation

--- profit_calculator.py
+++ profit_calculator.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
 
-rate = float(sys.argv[1])
-end = int(sys.argv[2])
-increment = int(sys.argv[3])
+#TODO: add argsparse module
+# parser = argparse.ArgumentParser()
+# parser.add_argument("a")
+# args = parser.parse_args()
+
+capital = int(sys.argv[1])
+rate = float(sys.argv[2])
+end = int(sys.argv[3])
+increment = int(sys.argv[4])
 
 for capital in range(0, end, increment):
     balance = capital * rate
